7f58e015b652c5259a79e11320ecf8adbc7711f3
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-04-24 14:02:29 +0530

    understanding and using state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    Persons: [
+      {name: 'ankit11', age: '29'}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I am react</h1>
         <p>This is really working</p>
-        <Person name="ankit">Hey bro!</Person>   
+        <Person name={this.state.Persons[0].name}>Hey bro!</Person>   
       </div>
     )
     // return React.createElement('div', {className: 'App'})
