551ec8b431b33390300ed04569fcc987796f980b
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-05-02 12:23:19 +0530

    adding subscription

--- src/redux-basics.js
+++ src/redux-basics.js
@@ -26,6 +26,11 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+//Subscription call whenever state will change
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState())
+})
+
 //Dispatching Action
 store.dispatch({
     type: 'INC_COUNTER'
@@ -37,4 +42,3 @@ store.dispatch({
 })
 console.log(store.getState())
 
-//Subscription
