b4a6666f1ddf8f33892e350d356c3bcad73d7a20
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-05-02 12:31:22 +0530

    inc dec project

--- redux-basics.js
+++ redux-basics.js
@@ -5,40 +5,33 @@ const initialState = {
     counter: 0
 }
 
-//Reducer
+// Reducer
 const rootReducer = (state = initialState, action) => {
     if (action.type === 'INC_COUNTER') {
         return {
             ...state,
             counter: state.counter + 1
-        }
+        };
     }
     if (action.type === 'ADD_COUNTER') {
         return {
             ...state,
             counter: state.counter + action.value
-        }
+        };
     }
-    return state
-}
+    return state;
+};
 
-// store
+// Store
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-//Subscription call whenever state will change
+// Subscription
 store.subscribe(() => {
-    console.log('[Subscription]', store.getState())
-})
+    console.log('[Subscription]', store.getState());
+});
 
-//Dispatching Action
-store.dispatch({
-    type: 'INC_COUNTER'
-})
-
-store.dispatch({
-    type: 'ADD_COUNTER', 
-    value: 10
-})
-console.log(store.getState())
-
+// Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
