af159bb2633e42e26f4d56d9a336859cb8922220
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-06-21 14:41:58 +0530

    styline the active route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -13,12 +13,16 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                pathname: this.props.match.url + '/new-post',
+                            {/* here class active will apply and want to change the class active we will use activeClassName*/}
+                            <li><NavLink to="/" exact activeClassName="my-active" activeStyle={{
+                                color: '#fa923f',
+                                textDecoration: 'underline'
+                            }}>Home</NavLink></li>  
+                            <li><NavLink to={{
+                                pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
