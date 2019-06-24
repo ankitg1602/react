de1c2c5acb329d7049352a5aef2f3243fb507d78
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-06-24 12:56:59 +0530

    creating dynamic nested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,8 +5,6 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import Post from './FullPost/FullPost'
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -15,11 +13,14 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            {/* here class active will apply and want to change the class active we will use activeClassName*/}
-                            <li><NavLink to="/" exact activeClassName="my-active" activeStyle={{
-                                color: '#fa923f',
-                                textDecoration: 'underline'
-                            }}>Home</NavLink></li>  
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
@@ -31,11 +32,9 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route path="/" exact component={Posts} />
                     <Route path="/new-post" component={NewPost} />
-                    <Route path="/:id" exact component={FullPost}/>
+                    <Route path="/posts" component={Posts} />
                 </Switch>
-              
             </div>
         );
     }
